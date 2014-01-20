8bb40f42def4e5c6598fe9042e618f01d68ffe31
Clint Stevenson <clintastevenson@gmail.com>
2014-01-19 22:42:57 -0600

    Cleaned up code and stylized for readability

--- Debug/main.pp
+++ Debug/main.pp
@@ -4,8 +4,10 @@ main.obj: ../main.c
 main.obj: C:/ti/ccsv5/ccs_base/msp430/include/msp430.h
 main.obj: C:/ti/ccsv5/ccs_base/msp430/include/msp430f169.h
 main.obj: C:/ti/ccsv5/ccs_base/msp430/include/in430.h
+main.obj: C:/ti/ccsv5/tools/compiler/msp430_4.1.5/include/stdint.h
 
 ../main.c: 
 C:/ti/ccsv5/ccs_base/msp430/include/msp430.h: 
 C:/ti/ccsv5/ccs_base/msp430/include/msp430f169.h: 
 C:/ti/ccsv5/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv5/tools/compiler/msp430_4.1.5/include/stdint.h: 
